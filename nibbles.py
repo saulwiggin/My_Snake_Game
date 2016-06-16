08b0f0099133f576c826fbb6cf30a78581f796ce
saul wiggin <saulwiggin@users.noreply.github.com>
2016-06-16 11:02:10 +0100

    update

--- nibbles.py
+++ nibbles.py
@@ -1,8 +1,3 @@
-# Wormy (a Nibbles clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
 import random, pygame, sys
 from pygame.locals import *
 
